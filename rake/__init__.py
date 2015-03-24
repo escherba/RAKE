aafc45407dc0b2c760ec2db33154d2cb51606adc
Eugene Scherba <escherba@gmail.com>
2015-03-24 02:14:17 -0700

    add packaging and a REPL-like shell

--- rake/__init__.py
+++ rake/__init__.py
@@ -1,14 +1,19 @@
 # Implementation of RAKE - Rapid Automtic Keyword Exraction algorithm
 # as described in:
-# Rose, S., D. Engel, N. Cramer, and W. Cowley (2010). 
-# Automatic keyword extraction from indi-vidual documents. 
+# Rose, S., D. Engel, N. Cramer, and W. Cowley (2010).
+# Automatic keyword extraction from indi-vidual documents.
 # In M. W. Berry and J. Kogan (Eds.), Text Mining: Applications and Theory.unknown: John Wiley and Sons, Ltd.
 
 import re
-import operator
+from pkg_resources import resource_filename
+from operator import itemgetter
 
-debug = False
-test = True
+# Fox stoplist contains "numbers", so it will not find "natural numbers" like in Table 1.1
+# STOPPATH = resource_filename(__name__, "FoxStoplist.txt")
+
+# SMART stoplist misses some of the lower-scoring keywords in Figure 1.5,
+# which means that the top 1/3 cuts off one of the 4.0 score words in Table 1.1
+STOPPATH = resource_filename(__name__, "SmartStoplist.txt")
 
 
 def is_number(s):
@@ -43,7 +48,8 @@ def separate_words(text, min_word_return_size):
     words = []
     for single_word in splitter.split(text):
         current_word = single_word.strip().lower()
-        #leave numbers in phrase, but don't count as words, since they tend to invalidate scores of their phrases
+        # leave numbers in phrase, but don't count as words,
+        # since they tend to invalidate scores of their phrases
         if len(current_word) > min_word_return_size and current_word != '' and not is_number(current_word):
             words.append(current_word)
     return words
@@ -59,7 +65,7 @@ def split_sentences(text):
     return sentences
 
 
-def build_stop_word_regex(stop_word_file_path):
+def build_stop_word_regex(stop_word_file_path=STOPPATH):
     stop_word_list = load_stop_words(stop_word_file_path)
     stop_word_regex_list = []
     for word in stop_word_list:
@@ -88,13 +94,13 @@ def calculate_word_scores(phraseList):
         word_list = separate_words(phrase, 0)
         word_list_length = len(word_list)
         word_list_degree = word_list_length - 1
-        #if word_list_degree > 3: word_list_degree = 3 #exp.
+        # if word_list_degree > 3: word_list_degree = 3 # exp.
         for word in word_list:
             word_frequency.setdefault(word, 0)
             word_frequency[word] += 1
             word_degree.setdefault(word, 0)
-            word_degree[word] += word_list_degree  #orig.
-            #word_degree[word] += 1/(word_list_length*1.0) #exp.
+            word_degree[word] += word_list_degree  # orig.
+            # word_degree[word] += 1.0 / word_list_length # exp.
     for item in word_frequency:
         word_degree[item] = word_degree[item] + word_frequency[item]
 
@@ -102,8 +108,8 @@ def calculate_word_scores(phraseList):
     word_score = {}
     for item in word_frequency:
         word_score.setdefault(item, 0)
-        word_score[item] = word_degree[item] / (word_frequency[item] * 1.0)  #orig.
-    #word_score[item] = word_frequency[item]/(word_degree[item] * 1.0) #exp.
+        word_score[item] = word_degree[item] / float(word_frequency[item])  # orig.
+    # word_score[item] = word_frequency[item] / float(word_degree[item]) # exp.
     return word_score
 
 
@@ -120,49 +126,13 @@ def generate_candidate_keyword_scores(phrase_list, word_score):
 
 
 class Rake(object):
-    def __init__(self, stop_words_path):
-        self.stop_words_path = stop_words_path
-        self.__stop_words_pattern = build_stop_word_regex(stoppath)
+    def __init__(self, stop_words_path=STOPPATH):
+        self.__stop_words_pattern = build_stop_word_regex(stop_words_path)
 
     def run(self, text):
         sentence_list = split_sentences(text)
-
         phrase_list = generate_candidate_keywords(sentence_list, self.__stop_words_pattern)
-
         word_scores = calculate_word_scores(phrase_list)
-
         keyword_candidates = generate_candidate_keyword_scores(phrase_list, word_scores)
-
-        sorted_keywords = sorted(keyword_candidates.iteritems(), key=operator.itemgetter(1), reverse=True)
+        sorted_keywords = sorted(keyword_candidates.iteritems(), key=itemgetter(1), reverse=True)
         return sorted_keywords
-
-
-if test:
-    text = "Compatibility of systems of linear constraints over the set of natural numbers. Criteria of compatibility of a system of linear Diophantine equations, strict inequations, and nonstrict inequations are considered. Upper bounds for components of a minimal set of solutions and algorithms of construction of minimal generating sets of solutions for all types of systems are given. These criteria and the corresponding algorithms for constructing a minimal supporting set of solutions can be used in solving all the considered types of systems and systems of mixed types."
-
-    # Split text into sentences
-    sentenceList = split_sentences(text)
-    #stoppath = "FoxStoplist.txt" #Fox stoplist contains "numbers", so it will not find "natural numbers" like in Table 1.1
-    stoppath = "SmartStoplist.txt"  #SMART stoplist misses some of the lower-scoring keywords in Figure 1.5, which means that the top 1/3 cuts off one of the 4.0 score words in Table 1.1
-    stopwordpattern = build_stop_word_regex(stoppath)
-
-    # generate candidate keywords
-    phraseList = generate_candidate_keywords(sentenceList, stopwordpattern)
-
-    # calculate individual word scores
-    wordscores = calculate_word_scores(phraseList)
-
-    # generate candidate keyword scores
-    keywordcandidates = generate_candidate_keyword_scores(phraseList, wordscores)
-    if debug: print keywordcandidates
-
-    sortedKeywords = sorted(keywordcandidates.iteritems(), key=operator.itemgetter(1), reverse=True)
-    if debug: print sortedKeywords
-
-    totalKeywords = len(sortedKeywords)
-    if debug: print totalKeywords
-    print sortedKeywords[0:(totalKeywords / 3)]
-
-    rake = Rake("SmartStoplist.txt")
-    keywords = rake.run(text)
-    print keywords
